fe0def1b3d9c78328b2246a2e5634972856cb51c
Zachary Hill <zach.hill@me.com>
2026-04-27 16:16:00 -0400

    Springboot app finished

--- 02-deploy-backend-application/items/src/main/java/com/revature/items/controller/itemController.java
+++ 02-deploy-backend-application/items/src/main/java/com/revature/items/controller/itemController.java
@@ -1,4 +1,40 @@
 package com.revature.items.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class itemController {
+
+    private final List<String> items = new ArrayList<>();
+
+    @GetMapping("/items")
+    public List<String> getItems(){return items;}
+
+    @GetMapping("/items/{id}")
+    public String getItemById(@PathVariable int id){
+        return items.get(id);
+    }
+
+    @PostMapping("/items")
+    public String addItem(@RequestBody String item){
+        System.out.println("Added " + item + "to list");
+        items.add(item);
+        return item;
+    }
+
+    @PutMapping("items/{id}")
+    public String putItem(@RequestBody String item, @PathVariable int id){
+        System.out.println("Changed to " + item);
+        items.set(id, item);
+        return item;
+    }
+
+    @DeleteMapping("items/{id}")
+    public String removeItem(@PathVariable int id){
+        String item = items.remove(id);
+        return ("Removed " + item + " from list");
+    }
 }
