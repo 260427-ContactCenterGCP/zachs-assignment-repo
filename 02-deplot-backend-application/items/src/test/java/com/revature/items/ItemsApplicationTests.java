28b2e4e3573391efeb590efdec7d8ed0895b6873
Zachary Hill <zach.hill@me.com>
2026-04-27 15:47:37 -0400

    Springboot App added

---
+++ 02-deplot-backend-application/items/src/test/java/com/revature/items/ItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
